b615b947b36c10fb4c85679bef1674538bbe0727
AbdAlwahab <bayanadam10@gmail.com>
2024-07-24 11:28:53 +0300

    Adding the header

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
 import Sidebar from "./_components/Sidebar";
+import CurrentUserBadge from "./_components/CurrentUserBadge";
 
 const sourceSans3 = Source_Sans_3({ subsets: ["latin"] });
 
@@ -23,7 +24,13 @@ export default function RootLayout({
         className={`${sourceSans3.className} grid h-screen grid-cols-[20rem_1fr] gap-3 overflow-hidden bg-white p-4`}
       >
         <Sidebar />
-        <main>{children}</main>
+
+        <div className="flex h-full flex-col gap-4">
+          <header className="flex justify-end rounded-lg bg-[#FBB13C] px-6 py-4">
+            <CurrentUserBadge />
+          </header>
+          <main className="flex-1">{children}</main>
+        </div>
       </body>
     </html>
   );
