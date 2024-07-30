2f9f3bad198e561ddeb58a767e9313c804ecb9d8
AbdAlwahab <bayanadam10@gmail.com>
2024-07-31 00:01:32 +0300

    Adding home page stats

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,33 @@
 import { cache } from 'react'
-import { getTasksCount } from './_services/tasksServices'
+import { getTasks } from './_services/tasksServices'
 import { FaChevronRight, FaTasks } from 'react-icons/fa'
-import { getCategoriesCount } from './_services/categoriesService'
+import { getCategories } from './_services/categoriesService'
 import { BiCategory } from 'react-icons/bi'
 import Link from 'next/link'
 
 export default async function Home() {
-  const tasksPromise = cache(async () => await getTasksCount())
+  const tasksPromise = cache(async () => await getTasks())
   const tasks = await tasksPromise()
 
-  const categoriesPromise = cache(async () => await getCategoriesCount())
+  const categoriesPromise = cache(async () => await getCategories())
   const categories = await categoriesPromise()
 
+  const today = new Date()
+  today.setHours(0, 0, 0, 0) // Start of today
+
+  const tasksCreatedToday = tasks?.filter((task) => {
+    const createdAt = new Date(task.created_at)
+    return createdAt >= today
+  })
+
+  const categoriesCreatedToday = categories?.filter((task) => {
+    const createdAt = new Date(task.created_at)
+    return createdAt >= today
+  })
+
   return (
-    <div className="h-full bg-gray-100 shadow-lg">
-      <div className="grid grid-cols-2 gap-4 p-4">
+    <div className="flex h-full flex-col gap-4 bg-gray-100 p-4 shadow-lg">
+      <div className="grid grid-cols-2 gap-4">
         <Link
           className="transition-all hover:translate-y-[-3px] hover:border-curious-blue-800 hover:shadow-xl"
           href="/planner"
@@ -60,6 +73,65 @@ export default async function Home() {
             </div>
           </div>
         </Link>
+      </div>
+
+      <div className="h-[400px] rounded-lg bg-gray-800">
+        {tasksCreatedToday?.length ? (
+          <div className="flex flex-col gap-5 p-8">
+            <span className="text-3xl font-semibold text-white">Today</span>
+
+            <div className="grid grid-cols-2 gap-4">
+              <div className="grid h-fit max-h-[400px] grid-cols-1  gap-4 overflow-y-auto">
+                {tasksCreatedToday?.map((task) => (
+                  <div
+                    key={task.id}
+                    className="flex h-fit items-center gap-2 rounded-lg bg-curious-blue-800 p-3"
+                  >
+                    <div>
+                      <FaTasks fontSize={18} color="white" />
+                    </div>
+                    <span className="font-semibold text-gray-300">
+                      {task.title}
+                    </span>
+
+                    <span className="ml-auto rounded-lg bg-red-600 px-2 py-1 text-sm font-semibold text-gray-300">
+                      {
+                        categories?.find(
+                          (category) => category.id === task.category,
+                        )?.name
+                      }
+                    </span>
+                  </div>
+                ))}
+              </div>
+
+              <div className="grid h-fit max-h-[400px] grid-cols-1 gap-4  overflow-y-auto">
+                {categoriesCreatedToday?.map((category) => (
+                  <div
+                    key={category.id}
+                    className="flex h-fit items-center gap-2 rounded-lg bg-curious-blue-800 p-3"
+                  >
+                    <div>
+                      <BiCategory fontSize={18} color="white" />
+                    </div>
+                    <span className="font-semibold text-gray-300">
+                      {category.name}
+                    </span>
+
+                    <span className="ml-auto rounded-lg bg-red-600 px-2 py-1 text-sm font-semibold text-gray-300">
+                      {
+                        tasks?.filter((task) => task.category === category.id)
+                          ?.length
+                      }
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        ) : (
+          <span> No tasks created today</span>
+        )}
       </div>
     </div>
   )
