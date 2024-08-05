24790e415e54cdf768baf7be53bc00e1cf74d58b
AbdAlwahab <bayanadam10@gmail.com>
2024-08-05 11:22:28 +0300

    Refactor the categories pages and remove loading.tsx from categories

---
+++ app/_features/categories/CategoryListContainer.tsx
@@ -0,0 +1,72 @@
+import Menus from '@/app/_components/Menus'
+import Modal, { OpenModal, ModalWindow } from '@/app/_components/Modal'
+import { getCategories } from '@/app/_services/categoriesService'
+import { getCategoriesTasks } from '@/app/_services/tasksServices'
+import { HiOutlinePlus } from 'react-icons/hi2'
+import { RiErrorWarningFill } from 'react-icons/ri'
+import CategoriesIIndexList from './CategoriesIIndexList'
+import CreateNewCategoryForm from './CreateNewCategoryForm'
+
+const MAX_CATEGORIES = 10
+
+async function CategoryListContainer() {
+  const [categories, categoriesTasks] = await Promise.all([
+    getCategories(),
+    getCategoriesTasks(),
+  ])
+
+  return (
+    <Menus>
+      <div className="flex h-full flex-col gap-4 bg-gray-100">
+        <div className="flex items-center justify-between rounded-lg bg-white px-1 py-2 lg:px-5">
+          <h1 className="text-xl font-bold lg:text-3xl">Categories</h1>
+
+          <Modal>
+            <OpenModal name="add-category">
+              <button className="flex items-center gap-1 rounded-lg bg-gray-900 px-2 py-1.5 text-white lg:px-4 lg:py-3">
+                <span className="text-xs lg:text-base"> New Category</span>
+                <HiOutlinePlus fontSize={20} />
+              </button>
+            </OpenModal>
+
+            <ModalWindow
+              name="add-category"
+              label={
+                categories?.length === MAX_CATEGORIES
+                  ? 'Reach the maximum number of categories'
+                  : 'Create New Category'
+              }
+            >
+              {categories?.length === MAX_CATEGORIES ? (
+                <div className="flex flex-col items-center justify-center gap-4 text-xl">
+                  <RiErrorWarningFill fontSize={80} color="red" />
+                  <span>
+                    You have already created the maximum number of{' '}
+                    {MAX_CATEGORIES} categories.
+                  </span>
+                </div>
+              ) : (
+                <CreateNewCategoryForm />
+              )}
+            </ModalWindow>
+          </Modal>
+        </div>
+
+        {categories?.length ? (
+          <CategoriesIIndexList
+            categories={categories}
+            categoriesTasks={categoriesTasks ?? []}
+          />
+        ) : (
+          <div className="flex size-full flex-col items-center justify-center gap-8">
+            <span className="text-2xl font-semibold lg:text-4xl">
+              No categories found
+            </span>
+          </div>
+        )}
+      </div>
+    </Menus>
+  )
+}
+
+export default CategoryListContainer
--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,71 +1,11 @@
-import { getCategories } from '../_services/categoriesService'
-import Menus from '../_components/Menus'
-import { HiOutlinePlus } from 'react-icons/hi2'
-import Modal, { ModalWindow, OpenModal } from '../_components/Modal'
-import CreateNewCategoryForm from '../_features/categories/CreateNewCategoryForm'
-import CategoriesIIndexList from '../_features/categories/CategoriesIIndexList'
-import { getCategoriesTasks } from '../_services/tasksServices'
-import { RiErrorWarningFill } from 'react-icons/ri'
-
-const MAX_CATEGORIES = 10
+import { Suspense } from 'react'
+import CategoryListContainer from '../_features/categories/CategoryListContainer'
 
 async function Categories() {
-  const [categories, categoriesTasks] = await Promise.all([
-    getCategories(),
-    getCategoriesTasks(),
-  ])
-
   return (
-    <Menus>
-      <div className="flex h-full flex-col gap-4 bg-gray-100">
-        <div className="flex items-center justify-between rounded-lg bg-white px-1 py-2 lg:px-5">
-          <h1 className="text-xl font-bold lg:text-3xl">Categories</h1>
-
-          <Modal>
-            <OpenModal name="add-category">
-              <button className="flex items-center gap-1 rounded-lg bg-gray-900 px-2 py-1.5 text-white lg:px-4 lg:py-3">
-                <span className="text-xs lg:text-base"> New Category</span>
-                <HiOutlinePlus fontSize={20} />
-              </button>
-            </OpenModal>
-
-            <ModalWindow
-              name="add-category"
-              label={
-                categories?.length === MAX_CATEGORIES
-                  ? 'Reach the maximum number of categories'
-                  : 'Create New Category'
-              }
-            >
-              {categories?.length === MAX_CATEGORIES ? (
-                <div className="flex flex-col items-center justify-center gap-4 text-xl">
-                  <RiErrorWarningFill fontSize={80} color="red" />
-                  <span>
-                    You have already created the maximum number of{' '}
-                    {MAX_CATEGORIES} categories.
-                  </span>
-                </div>
-              ) : (
-                <CreateNewCategoryForm />
-              )}
-            </ModalWindow>
-          </Modal>
-        </div>
-
-        {categories?.length ? (
-          <CategoriesIIndexList
-            categories={categories}
-            categoriesTasks={categoriesTasks ?? []}
-          />
-        ) : (
-          <div className="flex size-full flex-col items-center justify-center gap-8">
-            <span className="text-2xl font-semibold lg:text-4xl">
-              No categories found
-            </span>
-          </div>
-        )}
-      </div>
-    </Menus>
+    <Suspense fallback={<div>Loading...</div>}>
+      <CategoryListContainer />
+    </Suspense>
   )
 }
 
