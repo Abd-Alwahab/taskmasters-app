b2655dd8b99f6bc1fe63950ff698a534408ae18a
AbdAlwahab <bayanadam10@gmail.com>
2024-08-16 13:45:24 +0300

    Adding the feedbacks icons

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link'
 import { createClient } from '../_utils/supabase/server'
 import CurrentUserBadge from './CurrentUserBadge'
+import { FcFeedback } from 'react-icons/fc'
 
 async function Header() {
   const {
@@ -8,7 +10,14 @@ async function Header() {
 
   if (!user) return null
   return (
-    <header className="mb-2.5 flex justify-end rounded-lg bg-white px-6 py-4">
+    <header className="mb-2.5 flex justify-between rounded-lg bg-white px-6 py-4">
+      <Link
+        href="/feedbacks"
+        className="flex items-center gap-2 rounded-lg bg-gray-100 p-2"
+      >
+        <FcFeedback fontSize={24} />
+        <span>Feedbacks</span>
+      </Link>
       <CurrentUserBadge />
     </header>
   )
