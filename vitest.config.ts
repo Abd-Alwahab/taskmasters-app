fb65ddd14fb7596b9116be16f02a099f2f8fe9ff
AbdAlwahab <bayanadam10@gmail.com>
2024-09-01 13:19:46 +0300

    Adding path alias to viest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,17 @@
+import path from 'path'
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: ['tests/setupTests.ts'],
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
   },
 })
